c91fd457a333311d440854151e7ba930c121ce4c
Hsueh <40841121@gm.nfu.edu.tw>
2021-05-22 03:37:57 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/nfu/hsueh/tsai_councillor/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package tw.edu.nfu.hsueh.tsai_councillor.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
